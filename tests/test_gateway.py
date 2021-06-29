f7393d7e2b2f466607a2cef8edd9384e9a780cff
Kristen Thyng <kristen@axds.co>
2021-06-29 16:27:25 -0500

    more precommit

--- tests/test_gateway.py
+++ tests/test_gateway.py
@@ -1,8 +1,6 @@
 import cf_xarray
 import numpy as np
 import pint_xarray
-
-# import ocean_data_gateway as odg
 import xarray as xr
 
 from cf_xarray.units import units
@@ -16,9 +14,3 @@ def test_units():
     ds["salt"] = ("dim", np.arange(10), {"units": "psu"})
     ds["lat"] = ("dim", np.arange(10), {"units": "degrees_north"})
     assert ds.pint.quantify()
-
-
-# def test_QC():
-#     ds = xr.Dataset()
-#     ds["salt"] = ("dim", np.arange(10), {"units": "psu"})
-#     assert ds.pint.quantify()
