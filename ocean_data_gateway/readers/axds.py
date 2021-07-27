840a57c2de1830ad516e03fee375afd1bb151c94
Kristen Thyng <kristen@axds.co>
2021-07-27 12:18:54 -0500

    Fixes to axds reader
    * `engine='h5netcdf'` had previously fixed a problem when reading in with `fsspec` but now led to a problem so it has been commented out for now
    * figured out a better way to generalize the dimension swap that needs to happen so that time is a dimension and can be used properly with `cf-xarray`.

--- ocean_data_gateway/readers/axds.py
+++ ocean_data_gateway/readers/axds.py
@@ -673,7 +673,12 @@ sources:
                 # this downloads the http-served file to cache I think
                 download_url = self.catalog[dataset_id].urlpath
                 infile = fsspec.open(f"simplecache::{download_url}")
-                data = xr.open_dataset(infile.open()).swap_dims({"profile": "time"})
+                data = xr.open_dataset(infile.open())#, engine='h5netcdf')
+                # we need 'time' as a dimension for the subsequent line to work
+                dim = [dim for dim, size in data.dims.items() if size == data.cf['T'].size]
+                if len(dim) > 0:
+                    data = data.swap_dims({dim[0]: data.cf['T'].name})
+                # .swap_dims({"profile": "time"})
                 # filter by time
                 data = data.cf.sel(T=slice(self.kw["min_time"], self.kw["max_time"]))
 
